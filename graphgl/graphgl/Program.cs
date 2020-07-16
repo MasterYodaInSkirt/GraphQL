37a57d9f930f2f91fc54be176da96455763a3999
marijana.markov <marijana.z.markov@gmail.com>
2020-07-16 19:20:33 +0200

    initial commit

---
+++ graphgl/graphgl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace graphgl
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
