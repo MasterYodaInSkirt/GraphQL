80b8a0691bba266f90be8790fcb3d2ac9f9a826e
marijana.markov <marijana.z.markov@gmail.com>
2020-07-16 20:22:40 +0200

    implementation with playground

--- graphgl/graphgl/Program.cs
+++ graphgl/graphgl/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using GraphQL;
+using GraphQL.Types;
 
 namespace graphgl
 {
@@ -7,6 +9,31 @@ namespace graphgl
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var schema = Schema.For(@"
+              type Jedi {
+                  name: String,
+                  side: String,
+                  id: ID
+              }
+
+              type Query {
+                  hello: String,
+                  jedis: [Jedi],
+                  jedi(id: ID): Jedi
+              }
+              ", _ =>
+                        {
+                            _.Types.Include<Query>();
+                        });
+
+            var root = new { Hello = "Hello World!" };
+            var json = schema.Execute(_ =>
+            {
+                _.Query = "{ jedi(id: 1) { name } }";
+                _.Root = root;
+            });
+
+            Console.WriteLine(json);
         }
     }
 }
---
+++ graphgl/graphgl/Query.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using GraphQL;
+using System.Linq;
+
+public class Query
+{
+    [GraphQLMetadata("jedis")]
+    public IEnumerable<Jedi> GetJedis()
+    {
+        return new List<Jedi>{
+            new Jedi(){ Name ="Luke", Side="Light", Id = 1},
+            new Jedi(){ Name ="Yoda", Side="Light", Id = 2},
+            new Jedi(){ Name ="Darth Vader", Side="Dark", Id = 3}
+        };
+    }
+    // GraphQLMetadata -> attribute to customize the mapping to the specific schema type
+    [GraphQLMetadata("hello")]
+    public string GetHello()
+    {
+        return "Hello Query class";
+    }
+
+    [GraphQLMetadata("jedi")]
+    public Jedi GetJedi(int id)
+    {
+        var jedis = GetJedis();
+        return jedis.SingleOrDefault(x => x.Id == id );
+    }
+
+}
